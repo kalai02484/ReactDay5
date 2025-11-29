1a16a33e337792cbcf2ebd4d85248892a62e62fd
kalai02484 <kalai02484@gmail.com>
2025-11-30 01:13:51 +0530

    React Form Completed

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,34 +1,84 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Forms = () => {
+  const [formData, setFormData] = useState({
+    name: "Kalai",
+    email: "",
+    gender: "other",
+    password: "",
+  });
 
-    const [formData, setFormData] = useState({
-        name: 'Kalai',
-        email: '',
-        gender: 'other',
-        password: ''
+
+  const handleChange = (e) => {
+    //console.log(e.target.value);
+
+    const { name, value } = e.target;
+    //console.log(name, value);
+    setFormData({
+      ...formData,
+      [name]: value,
     });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+  };
 
   return (
     <div>
-        <h1>Form component</h1>
-        <form>
-            <label htmlFor="name">Name: </label>
-            <input type="text" id="name" name="name" value={formData.name} required /><br /><br />
-            <label htmlFor="email">Email: </label>
-            <input type="email" id="email" name="email" value={formData.email}  required /><br /><br />
-            <label htmlFor='gender' >Gender: </label>
-            <select id="gender" name="gender" value={formData.gender}  required >
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-            </select><br /><br />   
-            <label htmlFor="password">Password: </label>
-            <input type="password" id="password" name="password" required /><br /><br />
-            <input type="submit" value="Submit" />
-        </form>      
+      <h1>Form component</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <br />
+        <label htmlFor="gender">Gender: </label>
+        <select
+          id="gender"
+          name="gender"
+          value={formData.gender}
+          onChange={handleChange}
+          required
+        >
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+        <br />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={handleChange}
+          required
+        />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Forms
+export default Forms;
